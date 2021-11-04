7987b9bae6dcfd8064bca7bd1bf484aff9abb0d9
nicross <nicross@users.noreply.github.com>
2021-11-04 18:02:08 -0500

    Define system managing pack redemption

---
+++ src/js/content/packs.js
@@ -0,0 +1,37 @@
+content.packs = (() => {
+  let nextRedeem = 1
+
+  function calculateNextRedeem() {
+    let result = 4
+
+    while (result <= nextRedeem) {
+      result *= 2
+    }
+
+    return result
+  }
+
+  return {
+    canRedeem: (round = 0) => round >= nextRedeem,
+    getNextRedeem: () => nextRedeem,
+    getNextRedeemDelta: (round = 0) => Math.max(nextRedeem - round, 0),
+    redeem: (round = 0) => {
+      if (!this.canRedeem()) {
+        return
+      }
+
+      nextRedeem = calculateNextRedeem()
+
+      return round == 1
+        ? content.cards.generateStarterPack()
+        : content.cards.generateBoosterPack()
+    },
+    reset: function () {
+      nextRedeem = 1
+
+      return this
+    },
+  }
+})()
+
+engine.state.on('reset', () => content.packs.reset())
