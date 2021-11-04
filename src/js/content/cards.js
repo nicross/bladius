760fdceadc9354ef158c301d97d361d53b095ffe
nicross <nicross@users.noreply.github.com>
2021-11-03 21:16:09 -0500

    Generate starter/booster packs and store wares

--- src/js/content/cards.js
+++ src/js/content/cards.js
@@ -3,12 +3,86 @@ content.cards = (() => {
 
   return {
     generate: () => {
-      const archetype = engine.utility.chooseWeighted(registry, Math.random())
-      return archetype.generate()
+      const shuffled = engine.utility.shuffle(registry)
+      const card = engine.utility.chooseWeighted(shuffled, Math.random())
+      return {...card}
+    },
+    generateBoosterPack: (count = 3) => {
+      // Boosters consider rarity
+
+      const cards = [],
+        shuffled = engine.utility.shuffle(registry)
+
+      for (let i = 0; i < count; i += 1) {
+        const card = engine.utility.chooseWeighted(shuffled, Math.random())
+        cards.push({...card})
+      }
+
+      return cards
+    },
+    generateStarterPack: () => {
+      // Starter deck contains a little bit of everything
+
+      const isBoon = Math.random() > 0.5
+
+      const counts = {
+        Armor: 1,
+        Boon: isBoon ? 1 : 0,
+        Bane: isBoon ? 0 : 1,
+        Shield: 1,
+        Weapon: 3,
+      }
+
+      // Group cards by type
+      const cardsByType = {},
+        shuffled = engine.utility.shuffle(registry)
+
+      for (const card of shuffled) {
+        if (!card.isStarter) {
+          continue
+        }
+
+        const type = card.type
+
+        if (!cardsByType[type]) {
+          cardsByType[type] = []
+        }
+
+        cardsByType[type].push(card)
+      }
+
+      // Pick cards of each type
+      const cards = []
+
+      for (const type of Object.keys(counts)) {
+        const count = counts[type]
+
+        for (let i = 0; i < count; i += 1) {
+          cards.push(
+            engine.utility.chooseWeighted(cardsByType[type], Math.random())
+          )
+        }
+      }
+
+      return engine.utility.shuffle(cards)
+    },
+    generateStoreWares: (count = 3) => {
+      // The store doesn't consider weight when choosing cards, never has duplicates
+
+      const cards = [],
+        shuffled = engine.utility.shuffle(registry)
+
+      for (let i = 0; i < count; i += 1) {
+        const card = engine.utility.chooseSplice(shuffled, Math.random())
+        cards.push({...card})
+      }
+
+      return cards
     },
     register: function (definition) {
       registry.push(definition)
       return this
     },
+    registry: () => registry.map((card) => ({...card})),
   }
 })()
