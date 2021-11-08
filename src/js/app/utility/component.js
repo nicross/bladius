f7e4f0de45a35800b9d153c70d58ea059c6f4720
nicross <nicross@users.noreply.github.com>
2021-11-07 18:17:12 -0600

    Improve screen reader accessibility of experience/gold/potion components when within aria-live regions

--- src/js/app/utility/component.js
+++ src/js/app/utility/component.js
@@ -1,13 +1,13 @@
 app.utility.component = {}
 
 app.utility.component.experience = (count = 1) => {
-  return `<span class="c-experience">${count} <i aria-label="experience" title="Experience">✹</i></span>`
+  return `<span class="c-experience">${count} <i aria-hidden="true" role="presentation" title="Experience">✹</i><span class="u-screenReader">experience</span></span>`
 }
 
 app.utility.component.gold = (count = 1) => {
-  return `<span class="c-gold">${count} <i aria-label="gold" title="Gold">◈</i></span>`
+  return `<span class="c-gold">${count} <i aria-hidden="true" role="presentation" title="Gold">◈</i><span class="u-screenReader">gold</span></span>`
 }
 
 app.utility.component.potion = (count = 1) => {
-  return `<span class="c-potion">${count} <i aria-label="${count == 1 ? 'potion' : 'potions'}" title="Potion">✚</i></span>`
+  return `<span class="c-potion">${count} <i  aria-hidden="true" role="presentation" title="Potion">✚</i><span class="u-screenReader">${count == 1 ? 'potion' : 'potions'}</span></span>`
 }
