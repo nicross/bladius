18be4e6b81bcd7ae0f8f1a164a56e4b178e6c8d4
nicross <nicross@users.noreply.github.com>
2021-11-13 17:42:40 -0600

    Relabel next button on gold winnings prompt

--- src/js/app/component/prompt/gold.js
+++ src/js/app/component/prompt/gold.js
@@ -15,7 +15,7 @@ app.component.prompt.gold = app.component.prompt.base.invent({
 
     const button = document.createElement('button')
     button.className = 'c-button c-prompt--next'
-    button.innerHTML = 'Get Rest'
+    button.innerHTML = 'Get Good'
     button.addEventListener('click', (e) => this.onConfirm(e))
     this.rootElement.appendChild(button)
   },
