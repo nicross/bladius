0b778f39bdcc4d2a6c562f2a561271f75fb34a85
nicross <nicross@users.noreply.github.com>
2021-11-26 19:47:40 -0600

    Switch to planned graphical style

--- src/js/app/screen/fight/canvas.js
+++ src/js/app/screen/fight/canvas.js
@@ -1,10 +1,12 @@
 app.screen.fight.canvas = (() => {
-  const color = '#FF0069'
+  const drawDistance = 50,
+    vfov = engine.utility.degreesToRadians(90) * (9/16)
 
   let context,
     height,
-    mToPx,
+    hfov,
     particles = [],
+    particleRadius,
     root,
     width
 
@@ -15,11 +17,6 @@ app.screen.fight.canvas = (() => {
     window.addEventListener('resize', onResize)
     onResize()
 
-    content.collisions
-      .on('attack', onCollision)
-      .on('block', onCollision)
-      .on('parry', onCollision)
-
     app.state.screen.on('enter-fight', onEnter)
     app.state.screen.on('exit-fight', onExit)
   })
@@ -28,103 +25,81 @@ app.screen.fight.canvas = (() => {
     context.clearRect(0, 0, width, height)
   }
 
-  function collectCircles() {
-    const circles = []
-
-    for (const enemy of content.enemies.get()) {
-      const arm = enemy.arms.getActiveCollisionCircle(),
-        body = enemy.body.collisionCircle()
-
-      if (arm) {
-        arm.isArm = true
-        circles.push(arm)
-      }
-
-      circles.push(body)
-    }
-
-    const heroArm = content.hero.arms.getActiveCollisionCircle()
-
-    if (heroArm) {
-      heroArm.isArm = true
-      heroArm.isHero = true
-      circles.push(heroArm)
-    }
-
-    const angle = Math.PI - content.hero.body.angle,
-      position = content.hero.body.vector
-
-    for (const circle of circles) {
-      circle.relative = position.subtract(circle.vector).rotate(angle)
-    }
-
-    return circles
-  }
-
   function draw() {
-    const isPaused = engine.loop.isPaused()
-
     // Tracer effect
-    if (!isPaused) {
-      context.fillStyle = 'rgba(0, 0, 0, 0.5)'
-      context.fillRect(0, 0, width, height)
-    }
-
-    // Particles
-    context.fillStyle = context.strokeStyle = color
+    context.fillStyle = 'rgba(0, 0, 0, 0.5)'
+    context.fillRect(0, 0, width, height)
 
     for (const particle of particles) {
-      const radius = particle.radius * (1 - particle.life)
-
-      const x = (width / 2) - (particle.relative.y * mToPx),
-        y = (height / 2) - (particle.relative.x * mToPx)
-
-      context.fillRect(x - radius, y - radius, radius * 2, radius * 2)
-    }
-
-    // Circles
-    for (const circle of collectCircles()) {
-      context.strokeStyle = circle.isHero
-       ? '#FFFFFF'
-       : color
-
-      const x = (width / 2) - (circle.relative.y * mToPx),
-        y = (height / 2) - (circle.relative.x * mToPx)
-
-      context.beginPath()
-      context.arc(x, y, circle.radius * mToPx, 0, Math.PI * 2)
-
-      if (circle.isArm) {
-        context.stroke()
-      } else {
-        context.fill()
+      // Filter out particles behind field of view
+      if (particle.relative.x <= 0) {
+        continue
       }
-    }
 
-    // Player
-    context.fillStyle = '#FFFFFF'
-    context.beginPath()
-    context.arc(width / 2, height / 2, content.hero.body.radius * mToPx, 0, Math.PI * 2)
-    context.fill()
+      // Calculate distance
+      const distance = particle.relative.distance()
+
+      // Filter out nodes beyond draw distance
+      if (distance > drawDistance) {
+        continue
+      }
+
+      // Calculate horizontal position
+      const hangle = Math.atan2(particle.relative.y, particle.relative.x)
+
+      // Filter out particles beyond horizontal field of view (with leeway)
+      if (Math.abs(hangle) > hfov / 1.75) {
+        continue
+      }
+
+      // Calculate width
+      const radius = engine.utility.lerpExp(0, particleRadius, 1 - (distance / drawDistance), 8)
+
+      // Draw
+      const color = particle.isHero
+        ? `rgba(255, 255, 255, ${particle.life})`
+        : `rgba(255, 0, 105, ${particle.life})`
+
+      const x = (width / 2) - (width * hangle / hfov) - (radius / 2)
+      const y = (height - radius) * (particle.z)
+
+      context.fillStyle = color
+      context.fillRect(x, y, radius, radius)
+    }
   }
 
-  function generateParticle(vector) {
+  function generateParticle({
+    isHero,
+    x,
+    y,
+    z,
+  }) {
     const velocity = engine.utility.vector2d.unitX()
-      .scale(engine.utility.random.float(1, 10))
+      .scale(engine.utility.random.float(1/16, 1/4))
       .rotate(Math.PI * engine.utility.random.float(-1, 1))
 
     particles.push({
+      isHero,
       life: 1,
-      radius: engine.utility.random.float(1, 3),
       rotate: Math.PI/8 * engine.utility.random.float(-1, 1),
-      vector: vector.clone(),
+      vector: engine.utility.vector2d.create({x, y}),
       velocity,
+      z,
     })
   }
 
-  function generateParticles(vector, count = 0) {
-    for (let i = 0; i < count; i += 1) {
-      generateParticle(vector)
+  function generateParticles() {
+    for (const prop of engine.props.get()) {
+      if (content.prop.movement.isPrototypeOf(prop)) {
+        continue
+      }
+
+      generateParticle({
+        isHero: prop.isHero,
+        x: prop.x,
+        y: prop.y,
+        z: Math.random(),
+      })
     }
   }
 
@@ -139,21 +114,16 @@ app.screen.fight.canvas = (() => {
   }
 
   function onFrame() {
+    generateParticles()
     updateParticles()
     draw()
-  }
-
-  function onCollision({
-    where
-  } = {}) {
-    where = engine.utility.vector2d.create(where)
-    generateParticles(where, 32)
   }
 
   function onResize() {
     height = root.height = root.clientHeight
     width = root.width = root.clientWidth
-    mToPx = height / 25
+    hfov = vfov * (height / width)
+    particleRadius = 8 * (width / 1920)
   }
 
   function updateParticles() {
